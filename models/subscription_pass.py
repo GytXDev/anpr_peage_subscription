164ead5e7f6301a8bf1197134ac98ed0c1cd7e80
unknown <mahelnguindja@gmail.com>
2025-06-10 16:53:08 +0100

    This is a commit for today

--- models/subscription_pass.py
+++ models/subscription_pass.py
@@ -1,12 +1,20 @@
+# anpr_peage_subscription/models/subscription_pass.py
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class SubscriptionPass(models.Model):
     _name = 'anpr.subscription.pass'
     _description = 'Abonnement Péage'
 
     name = fields.Char(string="Nom de l'abonné", required=True)
-    user_id = fields.Many2one('res.users', string="Utilisateur lié", required=True)
     plate = fields.Char(string="Plaque d'immatriculation", required=True)
+    vehicle_type = fields.Selection([
+        ('car',     'Car'),
+        ('4x4',     '4x4 / SUV'),
+        ('bus',     'Bus'),
+        ('camion',  'Camion'),
+        ('autres',  'Autres'),
+    ], string="Type de véhicule", required=True)
     balance = fields.Float(string="Solde du compte", required=True, default=0.0)
     cost_per_passage = fields.Float(string="Coût par passage", required=True, default=500.0)
 
